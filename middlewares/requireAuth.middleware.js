db6cf6a758cc406f6e21d7ea8b603850240e9fc2
aharonmilman <milmana@gmail.com>
2022-10-02 01:21:18 +0300

    fixed auth servics didn't worked had the logg letters

--- middlewares/requireAuth.middleware.js
+++ middlewares/requireAuth.middleware.js
@@ -3,7 +3,6 @@ const logger = require('../services/logger.service')
 
 function requireAuth(req, res, next) {
   if (!req?.cookies?.loginToken) return res.status(401).send('Not Authenticated')
-  logg
   const loggedinUser = authService.validateToken(req.cookies.loginToken)
   if (!loggedinUser) return res.status(401).send('Not Authenticated')
   next()
